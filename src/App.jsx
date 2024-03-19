52ff31a51c21c6bcde7f12fac40047639fd25643
0MeMo07 <omemo0135@gmail.com>
2024-03-19 16:03:04 +0300

    fixed mobile loading issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,29 @@ import Footer2 from './components/Footer2'
 
 function App() {
   const [loading, setLoading] = useState(true);
+ 
   useEffect(() => {
-    window.onload = () => {
+    const handleLoad = () => {
       setLoading(false);
     };
 
+
+    window.onload = handleLoad;
+
+    const checkPageLoad = () => {
+      if (document.readyState === 'complete') {
+        setLoading(false);
+        window.onload = null;
+      }
+    };
+
+    const checkInterval = setInterval(checkPageLoad, 100);
+
     return () => {
+      clearInterval(checkInterval);
       window.onload = null;
     };
-  }, [])
+  }, []);
 
   return (
     <>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,22 +1,18 @@
-import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Home from '../components/Home'
 import About from '../components/About'
 import Menu from '../components/Menu'
 import Contact from '../components/Contact'
-import Loading from '../components/Loading'
 
 const AppRoutes = () => {
   return (
     <div>
-      <Suspense fallback={<Loading />}>
         <Routes>
           <Route path='/' exact element={<Home/>}/>
           <Route path='/about' exact element={<About/>}/>
           <Route path='/menu' exact element={<Menu/>}/>
           <Route path='/contact' exact element={<Contact/>}/>
         </Routes>
-      </Suspense>
     </div>
   )
 }
